27f94732c829e650c800734607369ab553de5b7a
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2024-03-06 12:52:40 -0300

    Exercício aula de useState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import  Garagem  from './Componentes/Garagem/Garagem'
+import { useState } from 'react'
+import Garagem from './Componentes/Garagem/Garagem'
 import { GlobalStyled } from './GlobalStyled'
 
+
 export default function App() {
+
+  const [nome, setNome] = useState('LuizZ')
+
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem nome={nome} setNome={setNome} />
     </div>
   )
 }
